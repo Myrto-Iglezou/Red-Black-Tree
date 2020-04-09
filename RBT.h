1496a933c13fa69c612ef256f5af3620eacc7b62
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2020-04-09 21:56:12 +0300

    comments

--- RBT.h
+++ RBT.h
@@ -35,3 +35,7 @@ void voted(Treenode *root,int *count);
 void printTree(FILE* f,Treenode * root);
 
 int comparator(const void* p1, const void* p2);
+// Return value meaning
+// <0 The element pointed by p1 goes before the element pointed by p2
+// 0  The element pointed by p1 is equivalent to the element pointed by p2
+// >0 The element pointed by p1 goes after the element pointed by p2
